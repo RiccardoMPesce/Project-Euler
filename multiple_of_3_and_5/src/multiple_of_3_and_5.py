2ca6cbc78d646b878a309c01ac071bd1ee0b9c23
riccardompesce <riccardompesce@gmail.com>
2020-12-03 22:40:54 +0100

    Adds docstring for first problem

--- multiple_of_3_and_5/src/multiple_of_3_and_5.py
+++ multiple_of_3_and_5/src/multiple_of_3_and_5.py
@@ -1,6 +1,6 @@
-import py
 def multiple_of_3_and_5(limit, test=False) -> int:
     """
-    
+    Given limit as an integer, returns the sum of all the multiple of
+    3 or 5 or both less than limit
     """
     return sum([i for i in range(limit) if i % 3 == 0 or i % 5 == 0])
