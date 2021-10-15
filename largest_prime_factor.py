2fe6aa5d46e0893fed9c16a9fc6c70fe8dec8abe
riccardompesce <riccardompesce@gmail.com>
2021-10-15 16:47:32 +0200

    I think I finalized the algorithm: it needs some checking with other numbers but it is fine for now as it gives out the correct result

--- largest_prime_factor.py
+++ largest_prime_factor.py
@@ -1,17 +1,22 @@
 def prime_factor_finder(n):
     factors = []
-
     last_div = 1
+    found = False
+
     while n > 1:
-        for i in range(last_div, n):
+        for i in range(last_div + 1, n):
             if n % i == 0:
+                found = True
                 factors += [i]
                 n //= i
-                last_div = i + 1
+                last_div = i
                 break
-        print(f"n is {n}")
+        if found:
+            found = False 
+        else:
+            break
 
-    return factors
+    return n
 
 def main():
     print(prime_factor_finder(600851475143))
